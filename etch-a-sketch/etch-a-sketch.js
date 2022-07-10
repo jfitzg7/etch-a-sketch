54cf63776d899d9990e3f051662f51bf4cf6fe61
jfitzg7 <jackf8088@gmail.com>
2022-07-09 22:23:41 -0600

    Finished the slidebar for setting the dimensions of the drawing area

--- etch-a-sketch/etch-a-sketch.js
+++ etch-a-sketch/etch-a-sketch.js
@@ -1,36 +1,34 @@
 const drawingArea = document.querySelector(".drawing-area");
 
 let colorMode = "";
+let drawingAreaDimension = 16;
 
-function createGridSquares(dimension) {
-  for (let i = 0; i < 16 * 16; i++) {
+function createGridSquares() {
+  drawingArea.innerHTML = "";
+  for (let i = 0; i < drawingAreaDimension ** 2; i++) {
     const gridSquare = document.createElement("div");
     gridSquare.classList.toggle("grid-square");
-    gridSquare.style.width = `${(1 / 16) * 100}%`;
-    gridSquare.style.height = `${(1 / 16) * 100}%`;
+    gridSquare.style.width = `${100 / drawingAreaDimension}%`;
+    gridSquare.style.height = `${100 / drawingAreaDimension}%`;
+    gridSquare.addEventListener("mouseover", () => {
+      if (colorMode === "rainbow") {
+        let randomR = Math.floor(Math.random() * 256);
+        let randomG = Math.floor(Math.random() * 256);
+        let randomB = Math.floor(Math.random() * 256);
+        gridSquare.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+      } else if (colorMode === "eraser") {
+        gridSquare.style.backgroundColor = "white";
+      } else {
+        gridSquare.style.backgroundColor = "black";
+      }
+    });
     drawingArea.appendChild(gridSquare);
   }
 }
 
 createGridSquares();
 
-const gridSquares = document.querySelectorAll(".grid-square");
-
-gridSquares.forEach((gridSquare) => {
-  gridSquare.addEventListener("mouseover", () => {
-    if (colorMode === "rainbow") {
-      let randomR = Math.floor(Math.random() * 256);
-      let randomG = Math.floor(Math.random() * 256);
-      let randomB = Math.floor(Math.random() * 256);
-      gridSquare.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
-    } else if (colorMode === "eraser") {
-      gridSquare.style.backgroundColor = "white";
-    } else {
-      gridSquare.style.backgroundColor = "black";
-    }
-  });
-});
-
+let gridSquares = document.querySelectorAll(".grid-square");
 const normalBtn = document.querySelector("#normal-btn");
 const rainbowBtn = document.querySelector("#rainbow-btn");
 const eraserBtn = document.querySelector("#eraser-btn");
@@ -61,4 +59,14 @@ clearBtn.addEventListener("click", () => {
   gridSquares.forEach((gridSquare) => {
     gridSquare.style.backgroundColor = "white";
   });
+});
+
+const dimensionSlider = document.querySelector("#dimension-slider");
+const dimensionSliderLabel = document.querySelector("#dimension-slider-label");
+
+dimensionSlider.addEventListener("input", () => {
+  dimensionSliderLabel.textContent = `${dimensionSlider.value} x ${dimensionSlider.value}`;
+  drawingAreaDimension = parseInt(dimensionSlider.value, 10);
+  createGridSquares();
+  gridSquares = document.querySelectorAll(".grid-square"); //need to reassign here so the clear button knows about the new grid squares
 });
